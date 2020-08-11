c2e85bad77ca241771a7093e220f0b075ad007c8
kangxufeng <530039987@qq.com>
2020-08-11 11:47:00 +0800

    version

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import Cookies from 'js-cookie'
 import control from './control';
 import container from './container';
 
+console.log('webview debugger version: 1.0.1')
+
 control(container.show.bind(container));
 
 if (Cookies.get('webview-debugger') === 'true') {
